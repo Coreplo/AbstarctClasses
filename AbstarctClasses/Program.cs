6f1931d7af9da935ee38519e7ca652603780b198
Coreplo <coreplo@gmail.com>
2019-06-25 18:17:55 +0200

    abstract class example

--- AbstarctClasses/Program.cs
+++ AbstarctClasses/Program.cs
@@ -10,12 +10,42 @@ namespace AbstarctClasses
     {
         static void Main(string[] args)
         {
-            // The code provided will print ‘Hello World’ to the console.
-            // Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
-            Console.WriteLine("Hello World!");
+
+            Shape c = new Circle();
+            c.Draw();  
             Console.ReadKey();
 
-            // Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app! 
+         }
+    }
+
+    abstract class Shape
+    {
+        public abstract void Draw();
+
+        //you cannot create objects with abstract methods 'cause abstract methods have no body(no implementation)
+        //having an abstract method means also that the class cannot be instanciated so it must be abstract its self;
+        //
+        // Abstract classes:
+        // - cannot be instantiated
+        // - ac's may contain abstract methods and accessors
+        // - Non Abstract classes derived from an abstract class must include actual implementations of all inherited
+        //   abstract methods and accessors
+        // - ofc abstract classes cannot be sealed :D
+    }
+
+    class Circle : Shape
+    {
+        public override void Draw()
+        {
+            Console.WriteLine("Spatascia un circle sul àutput");
+        }
+    }
+
+    class Rectangle : Shape
+    {
+        public override void Draw()
+        {
+            Console.WriteLine("Rect Draw");
         }
     }
 }
